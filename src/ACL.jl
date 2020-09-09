083d57419a1fb0931c70ef756cf06c6ac5c7df16
p1scescom <piscesuoza0317@gmail.com>
2020-09-10 00:29:28 +0900

    add dsu

--- src/ACL.jl
+++ src/ACL.jl
@@ -1,8 +1,19 @@
 module ACL
 
+using DataStructures, Primes
+using Base
+
 export
-    floorsum
+    #floorsum
+    floorsum,
+
+    #dsu
+    DSU,
+    leader,
+    same,
+    groups
 
 include("math/floorsum.jl")
+include("graph/dsu.jl")
 
 end
